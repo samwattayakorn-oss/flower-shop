dfee2e4d5b7a107d4ac431410868d9c8fb76586e
samwattayakorn-oss <sam.wattayakorn@gmail.com>
2026-06-24 20:19:57 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+/* ===================================================== */
+/* Flower Shop V1 */
+/* Main JavaScript */
+/* ===================================================== */
+
+console.log("Flower Shop V1 Loaded");
+
+/* Search Engine */
+
+function searchProduct(){
+
+const keyword =
+document.getElementById("search-input").value;
+
+console.log("Search :", keyword);
+
+}
+
+/* Cart System */
+
+function addToCart(productName){
+
+console.log(
+"Add To Cart : " + productName
+);
+
+}
+
+/* Buy Now */
+
+function buyNow(productName){
+
+console.log(
+"Buy Now : " + productName
+);
+
+}
